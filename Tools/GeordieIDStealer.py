bc8c03de84fe1ac9ec70637c3312537d4f3d92a7
Grmills01 <Grmills2002@gmail.com>
2025-01-25 14:25:32 +1030

    Updated GeordieIDStealer.py to be more user friendly, just run script and enter file address

--- Tools/GeordieIDStealer.py
+++ Tools/GeordieIDStealer.py
@@ -1,21 +1,40 @@
 import fileinput
+import os
 
-'''
-OPTIONS
-'''
+def get_file_path():
+    # Prompt the user for the file directory and ensure it exists
+    while True:
+        file_path = input("Please enter the file path: ").strip()
+        if os.path.isfile(file_path):
+            return file_path
+        else:
+            print("Invalid file path. Please try again.")
 
-fileURL = "./Arma 3 Preset 61st Mod List.html"
+def StealIDs(fileURL):
+    outputString = "serverMod=\""
+    with fileinput.FileInput(fileURL) as file:
+        for line in file:
+            if "<a href=" in line:
+                # Split based on quotation marks, get the steam id string
+                steamURL = line.split("\"")[1]
+                # Grab the id and append ';'
+                modID = "@" + str(steamURL.split("?id=")[1]) + ";"
+                outputString += str(modID)
+    print(outputString + "\"")
 
-def StealIDs():
-	outputString = "serverMod=\""
-	with fileinput.FileInput(fileURL) as file:
-		for line in file:
-			if "<a href=" in line:
-				#split based on quotation marks, get the steam id string
-				steamURL = line.split("\"")[1]
-				#grab the id and append ';'
-				modID = "@" + str(steamURL.split("?id=")[1]) + ";"
-				outputString += str(modID)
-	print(outputString + "\"")
+def main():
+    # Get the file URL from the user
+    fileURL = get_file_path()
+    StealIDs(fileURL)
 
-StealIDs()
+    # Ask the user if they want to quit or continue
+    while True:
+        quit_input = input("\nPress Enter to quit or type 'q' to exit and close: ").strip().lower()
+        if quit_input == 'q' or quit_input == '':
+            print("Exiting program. Goodbye!")
+            break
+        else:
+            print("Invalid input. Please press Enter to quit or type 'q' to quit.")
+
+# Run the main function
+main()
